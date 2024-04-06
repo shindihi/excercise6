47800c53bfe40aad1b43b13e5b46d45936d7e236
ChristianDinaVirgin <seanmichael.doinog@hcdc.edu.ph>
2024-04-06 10:39:34 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import '../styles/styles.css'; // Import the CSS file
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
